79fe1334de77c1b8da72b8fe0f4c4e8e84c16c9d
ReactExpert918 <potterharrry0918@gmail.com>
2021-02-09 12:49:19 -0800

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'dm-profile'
+import 'dm-profile/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
